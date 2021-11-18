176374f347c678d61dbeff213f0c52259a50208a
labeebalabi <59991923+labeebalabi@users.noreply.github.com>
2021-11-18 23:10:58 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,18 @@
+def rect() :
+    n=int(input(" side 1 : "))
+    m=int(input(" side 1 : "))
+    s=2*(n+m)
+    print("Area of rectangle : "+str(s))
+def tri() :
+    n=int(input(" base : "))
+    m=int(input(" height : "))
+    s=(n*m)/2
+    print("Area of triangle : "+str(s))
+def cir() :
+    r=int(input(" radius : "))
+    s=3.14*r*r
+    print("Area of circle : "+str(s))
+def squ() :
+    m=int(input(" side  : "))
+    s=a*a
+    print("Area of square : "+str(s))
